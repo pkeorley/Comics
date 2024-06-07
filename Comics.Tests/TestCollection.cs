93b9907c7d707650790b20ae61ac082c4cfc6f52
pkeorley <kolyaperedriy13@gmail.com>
2024-06-08 01:52:27 +0200

    feat: Implement new class named `Collection` with unit tests

---
+++ Comics.Tests/TestCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comics.Tests
+{
+    [TestClass]
+    public class TestCollection
+    {
+
+        [TestMethod]
+        public void TestCollectionClass()
+        {
+            Collection collection = new();
+
+            collection.Add(new Comic(150));
+            Assert.AreEqual(collection.TotalNumberOfPages, 150);
+
+            collection.Add(new Comic(100));
+            Assert.AreEqual(collection.TotalNumberOfPages, 250);
+
+            collection.Add(new Comic(1));
+            Assert.AreEqual(collection.TotalNumberOfPages, 251);
+        }
+
+        [TestMethod]
+        public void TestBadCollectionClass()
+        {
+            Collection collection = new();
+            
+            collection.Add(new Comic(-150));
+            Assert.AreEqual(collection.TotalNumberOfPages, 1);
+
+            collection.Add(new Comic(-1));
+            Assert.AreEqual(collection.TotalNumberOfPages, 2);
+
+            collection.Add(new Comic(0));
+            Assert.AreEqual(collection.TotalNumberOfPages, 3);
+        }
+
+    }
+}
---
+++ Comics/Collection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comics
+{
+    public class Collection
+    {
+        private List<Comic> _comics = new();
+        private int _totalNumberOfPages = 0;
+
+        public int TotalNumberOfPages
+        {
+            get {
+                return _totalNumberOfPages;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    _totalNumberOfPages = 0;
+                    return;
+                }
+                _totalNumberOfPages = value;
+            }
+        }
+    
+        public void Add(Comic comic)
+        {
+            _comics.Add(comic);
+            TotalNumberOfPages += comic.NumberPage;
+        }
+
+    }
+}
