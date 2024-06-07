6ceed3c1a6177ea6e888c95f9fa6e5d3ad63cdd9
pkeorley <kolyaperedriy13@gmail.com>
2024-06-07 20:16:41 +0200

    feat: Enhance `IComic` to inherit `IPaginable` and `ITimeLoadProvider` interfaces

--- Comics/ITimeLoanProvider.cs
+++ Comics/ITimeLoanProvider.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Comics
 {
-    public interface IComic : IPaginable, ITimeLoanProvider
+    public interface ITimeLoanProvider
     {
-       
+        double GiveTimeLoan();
     }
 }
